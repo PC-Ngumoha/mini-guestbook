276ac791fe2a453a162f16de3bcad4e9d4df2a01
PC-Ngumoha <pcngumoha@gmail.com>
2026-08-28 12:22:58 +0100

    Create seed script to run on GET

---
+++ lib/data.ts
@@ -0,0 +1,104 @@
+import { EntryType } from "./types";
+
+export const entries: Omit<EntryType, "id">[] = [
+  {
+    name: "Chinonso Okafor",
+    message:
+      "Babe, did you hear that Amaka and her so-called bestie aren't speaking anymore? Apparently, one of them has been dating the other's ex in secret.",
+  },
+  {
+    name: "Temilade Adebayo",
+    message:
+      "I just heard that Kunle proposed to Sade last weekend, but Sade allegedly told her friends she's not even sure she wants to marry him. 😭",
+  },
+  {
+    name: "Ifeoma Nwosu",
+    message:
+      "You didn't hear this from me, but apparently Ngozi got caught going through her husband's phone at 2am and found a whole second contact saved as 'Aunty Linda.'",
+  },
+  {
+    name: "Daniel Chukwuemeka",
+    message:
+      "Apparently Tobi has been telling everyone he's broke, meanwhile this guy just paid for a weekend trip to Dubai for someone he's not even officially dating.",
+  },
+  {
+    name: "Damilola Adeyemi",
+    message:
+      "Omo, the way Kemi suddenly stopped posting her boyfriend is suspicious. Her friends are saying there was a serious fight over another woman.",
+  },
+  {
+    name: "Adaeze Eze",
+    message:
+      "Please tell me you noticed that Yemi and Bisola unfollowed each other. Something definitely happened because those two used to post each other every other day.",
+  },
+  {
+    name: "Emmanuel Obinna",
+    message:
+      "Apparently Chidinma's new job wasn't exactly a normal application. Let's just say someone important may have made one phone call on her behalf. 👀",
+  },
+  {
+    name: "Teniola Balogun",
+    message:
+      "Girl, apparently Femi's 'female cousin' has been staying in his apartment for three months. Nobody has actually seen this cousin at any family event.",
+  },
+  {
+    name: "Nneka Uche",
+    message:
+      "I heard that Blessing and her sister are fighting because of inheritance money, and apparently one of them has receipts that could embarrass the whole family.",
+  },
+  {
+    name: "Oluwaseun Oladipo",
+    message:
+      "Apparently Segun told everyone he quit his job voluntarily, but according to his former colleague, HR had a very different story.",
+  },
+  {
+    name: "Kelechi Ibe",
+    message:
+      "You know that couple everyone thinks is perfect? Apparently they barely speak at home and only act romantic whenever they're around people.",
+  },
+  {
+    name: "Folake Ogunleye",
+    message:
+      "Babe, apparently Ronke's birthday dinner ended in tears because her boyfriend showed up with a gift meant for someone else. I need the full story!",
+  },
+  {
+    name: "Ikenna Maduka",
+    message:
+      "Apparently Uche has been secretly interviewing for jobs abroad while telling his boss he's completely committed to the company. The audacity is impressive.",
+  },
+  {
+    name: "Ayomide Fasanya",
+    message:
+      "I heard that Lara's engagement might be off, but her family is still pretending everything is fine because the wedding invitations already went out.",
+  },
+  {
+    name: "Ngozi Ekwueme",
+    message:
+      "Apparently Esther's new boyfriend used to date her roommate. Nobody told her until last night, and now the entire apartment is divided.",
+  },
+  {
+    name: "Yusuf Abdullahi",
+    message:
+      "Omo, apparently Ibrahim has been telling people he's single while posting his girlfriend on his close friends. One of his close friends clearly cannot keep secrets.",
+  },
+  {
+    name: "Bukola Akinyemi",
+    message:
+      "Apparently Funmi's luxury handbag collection is not quite what she makes it look like online. Someone claims half of them are borrowed for content. 😭",
+  },
+  {
+    name: "Chiamaka Nnamani",
+    message:
+      "I heard that Ada's best friend knows exactly why her last relationship ended, but she promised not to say anything. Now I'm dying to know.",
+  },
+  {
+    name: "Kayode Ajayi",
+    message:
+      "Apparently Deji's family is furious because he secretly got married months ago and only planned to tell them after the traditional wedding. Nigerian parents will not forgive that one.",
+  },
+  {
+    name: "Zainab Sani",
+    message:
+      "Babe, apparently the woman everyone thought was just his colleague is actually the reason their office relationship suddenly became 'strictly professional.' This gist is too much. 😭",
+  },
+];
---
+++ lib/types.ts
@@ -0,0 +1,5 @@
+export type EntryType = {
+  id: string;
+  name: string;
+  message: string;
+};
